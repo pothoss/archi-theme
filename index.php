93e7ee45ddbccdeba66267aa35c446eeb8be09d0
Antoine Haddaoui <hey@antoinehaddaoui.fr>
2018-06-25 19:54:35 +0200

    //TODO : add the menu

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<header class="my-logo">
-	<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo('name'); ?></a></h1>
+<header class="sticky-menu">
+    <!--//TODO: Add header menu here-->
 </header>
 <?php wp_nav_menu( array( 'header-menu' => 'header-menu' ) ); ?>
